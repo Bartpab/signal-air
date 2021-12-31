55aee4593615f9f4bc132420f224cabfd78ff1be
PABOIS Gaël <gael.pabois@jade.local>
2021-12-31 17:16:45 +0100

    up

--- lib/signalair/model/signalement/nuisance_olfactive.ex
+++ lib/signalair/model/signalement/nuisance_olfactive.ex
@@ -3,6 +3,8 @@ defmodule SignalAir.Signalement.NuisanceOlfactive do
     alias SignalAir.Signalement
 
     def __changeset__, do: Signalement.Commun.inherits_changeset type: :string,  intensite: :integer
+    
+    @derive Jason.Encoder
     defstruct Signalement.Commun.inherits_struct("nuisance_olfactive", type: nil, intensite: nil)
 
     def changeset(attrs \\ %{}) do
